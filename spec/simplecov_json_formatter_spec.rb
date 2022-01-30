11e868c6918eb060decb3920f6e28ce4df468245
Tobias Pfeiffer <pragtob@gmail.com>
2022-01-30 17:05:14 +0100

    Report groups overall coverage percentage

--- spec/simplecov_json_formatter_spec.rb
+++ spec/simplecov_json_formatter_spec.rb
@@ -55,5 +55,26 @@ describe SimpleCov::Formatter::JSONFormatter do
         expect(json_ouput).to eq(json_result('sample_with_branch'))
       end
     end
+
+    context 'with groups' do
+      let(:result) do
+        res = SimpleCov::Result.new({
+                                source_fixture('sample.rb') => { 'lines' => [
+                                  nil, 1, 1, 1, 1, nil, nil, 1, 1, nil, nil,
+                                  1, 1, 0, nil, 1, nil, nil, nil, nil, 1, 0, nil, nil, nil
+                                ] }
+                              })
+
+        # right now SimpleCov works mostly on global state, hence setting the groups that way
+        # would be global state --> Mocking is better here
+        allow(res).to receive_messages(groups: {"My Group" => double("File List", covered_percent: 80.0)})
+        res
+      end
+
+      it 'displays groups correctly in the JSON' do
+        subject.format(result)
+        expect(json_ouput).to eq(json_result('sample_groups'))
+      end
+    end
   end
 end
